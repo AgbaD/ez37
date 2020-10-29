35f543f76cc0d9bf64a1117e6954c5e684671089
Damilare <blankgodd33@gmail.com>
2020-10-29 16:09:43 +0100

    we move

---
+++ backend/config.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# Author: @AgbaD | @Agba_dr3
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'DFD04/4WD.3/43SD?S;D/SDT53432/DSDSD/32324/S434/34'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    KD_MAIL_SENDER = 'EZ37 Admin <admin@kd.com>'
+    KD_ADMIN = os.environ.get('EZ_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # SSL_DISABLE = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class Development(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    DEBUG = True
+
+
+class Testing(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    TESTING = True
+
+
+class Production(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': Development,
+    'testing': Testing,
+    'production': Production,
+    'default': Development
+}
